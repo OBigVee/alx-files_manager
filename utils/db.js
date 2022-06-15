faf122e5d155871703dc1a95274d73bce91bef09
OBigVee <adedejivictor98@gmail.com>
2022-06-15 21:52:36 +0100

    :heavy_check_mark: mongoDb connected

--- utils/db.js
+++ utils/db.js
@@ -1,18 +1,16 @@
-import mongodb from 'mongodb'
-import Collection from 'mongodb/lib/collection';
-import 'dotenv/config'
+import mongodb from 'mongodb';
+import 'dotenv/config';
 
-const dbClient = class DBClient {
+ const dbClient =  class DBClient {
     /**
      *
      */
-    host = process.env.DB_HOST //|| 'localhost';
-    port = process.env.DB_POST //|| 27017;
-    database = process.env.DB_DATABASE //|| 'files_manager';
-
     constructor() {
+        const host = process.env.DB_HOST //|| 'localhost';
+        const port = process.env.DB_POST //|| 27017;
+        const database = process.env.DB_DATABASE //|| 'files_manager';
         const uri = `mongodb://${this.host}:${this.port}/${this.database}`
-        this.client = new mongodb.MongoClient(this.uri)
+        this.client = new mongodb.MongoClient(uri, { useUnifiedTopology: true })
         this.client.connect()
     }
 
@@ -20,4 +18,5 @@ const dbClient = class DBClient {
         return this.client.isConnected();
     }
 }
-export default dbClient;
+
+export default new dbClient;
