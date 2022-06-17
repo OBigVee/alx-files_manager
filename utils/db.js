0dcdd2f3fd39fb48d5c522379571d17dcf1588e8
OBigVee <adedejivictor98@gmail.com>
2022-06-17 10:13:08 +0100

    :white_check_mark: MongoDB utils

--- utils/db.js
+++ utils/db.js
@@ -3,26 +3,26 @@ import 'dotenv/config';
 
 const dbClient = class DBClient {
     /**
-     *
+     *MongoDB utility
      */
     constructor() {
-            const host = process.env.DB_HOST || 'localhost';
-            const port = process.env.DB_POST || 27017;
-            const database = process.env.DB_DATABASE || 'files_manager';
+            const host = process.env.DB_HOST
+            const port = process.env.DB_POST
+            const database = process.env.DB_DATABASE
             const uri = `mongodb://${host}:${port}/${database}`
             this.client = new mongodb.MongoClient(uri, { useUnifiedTopology: true })
             this.client.connect()
         }
         /**
-         * 
-         * @returns 
+         * check if the mongodb connection is established
+         * @returns <Boolean>
          */
     isAlive() {
             return this.client.isConnected();
             //if (this.client.isConnected()) { return console.log('connected it is') }
         }
         /**
-         * 
+         * nbUsers returns the number of documents in the collection 'users'
          * @returns 
          */
     async nbUsers() {
